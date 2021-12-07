d387c8c2d76692c7d98df6c12031b7bfb2eca2de
Samuel Garneau <nomadev21@gmail.com>
2021-12-06 20:33:12 -0500

    changed startup class

--- runSim.sh
+++ runSim.sh
@@ -31,7 +31,7 @@ file="./results_$time"
 if ((resultLevel > 4)); then
     file="./results_$time.csv"
 fi
-cd "$localPath" && java -jar ./"$jarName" $meanDivider $maxQueueSize $maxTrial $resultLevel >"$file"
+cd "$localPath"/bin && java "$mainClass" $meanDivider $maxQueueSize $maxTrial $resultLevel >"$file"
 echo
 echo "Simulation completed"
 echo
