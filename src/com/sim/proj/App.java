b3eba768e42808e9c97c3b8e60428359469da804
GiaPrograms <legianguyen1999@gmail.com>
2021-12-06 21:28:22 -0500

    created an objective function result

--- src/com/sim/proj/App.java
+++ src/com/sim/proj/App.java
@@ -543,6 +543,9 @@ class App {
         System.out.println(YELLOW +
                 "Waiting time confidence interval (%): " + TEXT_RESET
                 + formatter.format(results.get("waitingTimeH")));
+        System.out.println(RED +
+                "Objective Functional: " + TEXT_RESET
+                + formatter.format(results.get("maxWaitingTime") + results.get("totalCost")));
         // system time stats
         System.out.println(BLUE + "Total system time average per executions (Y) (min): " + TEXT_RESET
                 + formatter.format(results.get("systemTime") / 60)
